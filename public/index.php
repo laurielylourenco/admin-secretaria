cc10632fce859499efc1aaf298a4697e24cbdbb8
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-05-24 22:47:28 -0300

    Ajuste para sessão ser salvar na pasta sessions, usuario de login vindo do banco de dados, correção link do botão de voltar na tela de criar turma

--- public/index.php
+++ public/index.php
@@ -4,6 +4,16 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
+
+// Define um caminho para salvar as sessões dentro do projeto
+// O __DIR__ aponta para a pasta 'public', então '../sessions' aponta para a pasta 'sessions' na raiz do projeto.
+$sessionPath = realpath(__DIR__ . '/../sessions');
+if (!is_dir($sessionPath)) {
+    mkdir($sessionPath, 0777, true); // Tenta criar se não existir
+}
+ini_set('session.save_path', $sessionPath);
+
+
 require_once '../config/config.php';
 require_once '../core/App.php';
  require_once '../core/Controller.php';
