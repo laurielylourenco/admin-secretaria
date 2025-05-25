c2507b2648ca7054c88ad5fe9282913e5059792d
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-05-25 13:15:54 -0300

    Remoção de display de erros

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
+/* error_reporting(E_ALL);
+ini_set('display_errors', 1); */
 
 // Define um caminho para salvar as sessões dentro do projeto
 // O __DIR__ aponta para a pasta 'public', então '../sessions' aponta para a pasta 'sessions' na raiz do projeto.
